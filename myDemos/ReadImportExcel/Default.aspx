43fa16129802af18cdfbc562c81b20f8615baaf9
Steven Mandel <smandel@enlivant.com>
2019-02-07 09:58:59 -0600

    added ReadImportExcelDemo

---
+++ myDemos/ReadImportExcel/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReadImportExcel._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+ 
+
+    <div class="row">
+        <div class="col-md-4">
+  
+            <br />
+            <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblEror" runat="server" Font-Bold="True" ForeColor="Red" Text="No File Selected for Upload" Visible="False"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Has Header ?" />
+
+<asp:RadioButtonList ID="rbHDR" runat="server">
+
+    <asp:ListItem Text = "Yes" Value = "Yes" Selected = "True" >
+
+    </asp:ListItem>
+
+    <asp:ListItem Text = "No" Value = "No"></asp:ListItem>
+
+</asp:RadioButtonList>
+            <br />
+            <br />
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+  
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+  
+        </div>
+    </div>
+
+</asp:Content>
---
+++ myDemos/ReadImportExcel/Default.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Web.UI;
+
+
+namespace ReadImportExcel
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            lblEror.Visible = false;
+
+            if (FileUpload1.HasFile)
+
+            {
+
+                string FileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+
+                string Extension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+
+                string FolderPath = ConfigurationManager.AppSettings["FolderPath"];
+
+                string FilePath = Server.MapPath(FolderPath + FileName);
+
+                FileUpload1.SaveAs(FilePath);
+
+                //       ReadAsDataTable(FilePath);
+
+                //               Import_To_Grid(FilePath, Extension, rbHDR.SelectedItem.Text);
+
+            }
+            else
+            {
+                lblEror.Visible = true;
+            }
+        }
+
+        /*
+        private void Import_To_Grid(string FilePath, string Extension, string isHDR)
+
+        {
+
+            string conStr = "";
+
+            switch (Extension)
+
+            {
+
+                case ".xls": //Excel 97-03
+
+                    conStr = ConfigurationManager.ConnectionStrings["Excel03ConString"]
+
+                             .ConnectionString;
+
+                    break;
+
+                case ".xlsx": //Excel 07
+
+                    conStr = ConfigurationManager.ConnectionStrings["Excel07ConString"]
+
+                              .ConnectionString;
+
+                    break;
+
+            }
+
+            conStr = String.Format(conStr, FilePath, isHDR);
+
+            OleDbConnection connExcel = new OleDbConnection(conStr);
+
+            OleDbCommand cmdExcel = new OleDbCommand();
+
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+
+            DataTable dt = new DataTable();
+
+            cmdExcel.Connection = connExcel;
+
+
+
+            //Get the name of First Sheet
+
+            connExcel.Open();
+
+            DataTable dtExcelSchema;
+
+            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            string SheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+
+            connExcel.Close();
+
+
+
+            //Read Data from First Sheet
+
+            connExcel.Open();
+
+            cmdExcel.CommandText = "SELECT * From [" + SheetName + "]";
+
+            oda.SelectCommand = cmdExcel;
+
+            oda.Fill(dt);
+
+            connExcel.Close();
+
+
+
+            //Bind Data to GridView
+
+            GridView1.Caption = Path.GetFileName(FilePath);
+
+            GridView1.DataSource = dt;
+
+            GridView1.DataBind();
+
+        }
+        */
+
+
+
+    }
+}
